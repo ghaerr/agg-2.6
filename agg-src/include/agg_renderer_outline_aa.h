acc3e96fca87845df9d4c024b8e8b896fe671572
Alex Fornuto <alex@fornuto.com>
2023-09-12 19:27:43 -0500

    Fix missing const declaration.
    I discovered this bug while trying to resolve https://github.com/TASEmulators/desmume/issues/729.

--- agg-src/include/agg_renderer_outline_aa.h
+++ agg-src/include/agg_renderer_outline_aa.h
@@ -1366,7 +1366,7 @@ namespace agg
         //---------------------------------------------------------------------
         void profile(line_profile_aa& prof) { m_profile = &prof; }
         const line_profile_aa& profile() const { return *m_profile; }
-        line_profile_aa& profile() { return *m_profile; }
+        const line_profile_aa& profile() { return *m_profile; }
 
         //---------------------------------------------------------------------
         int subpixel_width() const { return m_profile->subpixel_width(); }
